e302fef35299666948b6e067a1853faa1888e16f
Cezary Sanecki <cezary.sanecki@allegro.com>
2024-06-27 14:22:08 +0200

    Initial commit

---
+++ src/test/java/pl/cezarysanecki/gitapp/GitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.cezarysanecki.gitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
